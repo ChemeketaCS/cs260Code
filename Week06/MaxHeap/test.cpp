1aac1129badfdfb18dce373c9f090abd3dd55455
Andrew Scholer <ascholer@chemeketa.edu>
2021-06-16 11:50:22 -0700

    Update doctest to match version provided in updated UnitTest Project Template

--- Week06/MaxHeap/test.cpp
+++ Week06/MaxHeap/test.cpp
@@ -1,8 +1,17 @@
 //Bring in unit testing code and tell it to build a main function
+//If you have multiple unit test files, only one should have this define
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
 //This pragma supresses a bunch of warnings QTCreator produces (and should not)
-//#pragma clang diagnostic ignored "-Woverloaded-shift-op-parentheses"
+#pragma clang diagnostic ignored "-Woverloaded-shift-op-parentheses"
+
+//Hide some false warnings - this is supposed to look commented out
+// clazy:skip
+
+//Tell clang analyzer to ignore doctest.h
+#ifndef __clang_analyzer__
 #include "doctest.h"
+#endif
 
 //Use Approx from doctest without saying doctest::Approx
 using doctest::Approx;
